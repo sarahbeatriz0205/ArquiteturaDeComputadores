91d4b5110b3a6bc23b2f631b0f10b83f527c05a6
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2025-09-22 15:37:56 -0300

    questao 5

--- Lista1/questao5.asm
+++ Lista1/questao5.asm
@@ -1,1 +1,32 @@
+.text
 
+main: addi $2, $0, 5
+      syscall
+      add $8, $0, $2
+      
+      addi $9, $0, 10 # Constante
+      
+      div $8, $9
+      mflo $11 #atribui o quociente da divisão (que estã no reg LO) ao registrador $11
+      mfhi $12 #atribui o resto da divisão (que está no reg LO) ao registrador $12
+      add $10, $10, $12
+      sub $8, $0, $8
+      add $8, $0, $11
+      
+      div $8, $9
+      mflo $11
+      mfhi $12
+      add $10, $10, $12
+      sub $8, $0, $8
+      add $8, $0, $11 
+      
+      div $8, $9
+      mflo $11
+      mfhi $12
+      add $10, $10, $12
+      sub $8, $0, $8
+      add $8, $0, $11
+  
+      addi $2, $0, 1
+      add $4, $0, $10
+      syscall     
