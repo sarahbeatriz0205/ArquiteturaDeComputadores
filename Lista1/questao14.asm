6c992b4024c834b22482050825835a72aa594767
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2025-09-26 09:40:21 -0300

    questao 14

--- Lista1/questao14.asm
+++ Lista1/questao14.asm
@@ -5,4 +5,8 @@ main: addi $2, $0, 5
       
       addi $8, $0, 2
       div $2, $8
-      mfhi $15
+      mfhi $10
+      
+      addi $2, $0, 1
+      add $4, $0, $10
+      syscall
--- Lista1/questao6.asm
+++ Lista1/questao6.asm
@@ -1,6 +1,2 @@
-.text 
-
-main: addi $8, $0, 'h'
-	addi $2, $0, 4
-	syscall
+ 
 	
