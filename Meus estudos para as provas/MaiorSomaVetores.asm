c3e74faa00548c5ae6294f7d2dd83d203e26f3ca
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2025-11-14 14:56:15 -0300

    questao da 2ª prova

--- Meus estudos para as provas/MaiorSomaVetores.asm
+++ Meus estudos para as provas/MaiorSomaVetores.asm
@@ -6,13 +6,15 @@ main: # leituraEvetor
       addi $9, $0, 10
       lui $10, 0x1001
       lui $13, 0x1001
+       
       
       # soma
-      addi $11, $0, 0
-      addi $12, $0, 10
+      addi $2, $0, 5
+      syscall
+      addi $9, $2, -1
       
-      
-leituraEvetor: beq $8, $9, soma
+     
+leituraEvetor: beq $8, $9, comp
          addi $2, $0, 5
          syscall
          
@@ -22,52 +24,79 @@ leituraEvetor: beq $8, $9, soma
          
          addi $8, $8, 1
          j leituraEvetor
-
-soma: beq $11, $12, maior
+         
+comp: lui $13, 0x1001
       lw $14, 0($13)
       lw $15, 4($13)
-      addi $13, $13, 4 # garante que ele vai "percorrer todos os elementos" do vetor
-      add $14, $14, $15
-     
-      add $4, $0, $14
-      addi $2, $0, 1
-      syscall
+      add $16, $14, $15 #primeira soma
+      add $18, $0, $14 #primeiro valor
+      add $20, $0, $15 #segundo valor 
+      add $21, $0, $0
+      addi $11, $0, 1
+      addi $13, $13, 4
       
-      add $4, $0, ' '
-      addi $2, $0, 11
-      syscall
       
-      addi $11, $11, 1
-      j soma
-      
-      # zerando
-      add $8, $0, $0
-      add $9, $0, $0
-      add $11, $0, $0 
-      add $12, $0, $0
-      addi $12, $0, 10
-      addi $25, $0, 1
-      
-maior: beq $11, $12, sai
-       lw $8, 32($13) # maior inicial
-       addi $13, $13, -4
-       lw $9, 28($13)
-       
-       slt $24, $8, $9
-       beq $24, $25, substituirMaior
 
-       
-       addi $11, $11, 1
-       j maior
-
-# não tá acontecendo nada ainda       
-substituirMaior: add $8, $0, $9
-                 beq $11, $12, maior
+soma: beq $11, $12, sai
+      lw $14, 0($13)
+      lw $15, 4($13)
+      add $19, $14, $15
+      
+      sub $17, $16, $19
+      srl $17, $17, 31
+      beq $17, 1, substituirMaior
+      
+      j incrementoMaior
+      
+substituirMaior: 
+                 add $16, $0, $19
+                 add $18, $0, $14
+                 add $20, $0, $15
                  
-                 add $4, $0, $8
-                 addi $2, $0, 1
-                 syscall
+                 add $21, $0, $11
+                 
+
+
+incrementoMaior: addi $11, $11, 1
+                 addi $13, $13, 4 # garante que ele vai "percorrer todos os elementos" do vetor
+                 j soma
+                 
+    
+
        
 
-sai: addi $2, $0, 10
+sai: add $4, $0, $18
+     addi $2, $0, 1
+     syscall
+     
+     addi $4, $0, '+'
+     addi $2, $0, 11
+     syscall
+     
+     add $4, $0, $20
+     addi $2, $0, 1
+     syscall
+     
+     addi $4, $0, '='
+     addi $2, $0, 11
+     syscall
+
+     add $4, $0, $16
+     addi $2, $0, 1
+     syscall
+     
+     add $4, $0, '\n'
+     addi $2, $0, 11
+     syscall
+     
+     addi $4, $0, 'p'
+     addi $2, $0, 11
+     syscall
+     
+     
+     add $4, $0, $21
+     addi $2, $0, 1
+     syscall
+
+     addi $2, $0, 10
      syscall
